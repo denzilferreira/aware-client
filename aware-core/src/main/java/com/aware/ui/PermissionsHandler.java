81c756044628bdad5138bd2070b75c3c0b5491e1
Denzil Ferreira <ferreiradenzil@gmail.com>
2015-10-29 16:13:07 +0200

    - Fixed transparency on ESMs - WIP: PermissionsHandler transparency...

--- aware-core/src/main/java/com/aware/ui/PermissionsHandler.java
+++ aware-core/src/main/java/com/aware/ui/PermissionsHandler.java
@@ -22,9 +22,8 @@ public class PermissionsHandler extends Activity {
     @Override
     protected void onStart() {
         super.onStart();
-        //Fixes a crash on Android M for Activities with Theme.NoDisplay
         //c.f. https://code.google.com/p/android-developer-preview/issues/detail?id=2353
-        setVisible(true);
+//        setVisible(true);
 
         if( getIntent() != null && getIntent().getExtras() != null && getIntent().getStringArrayExtra(EXTRA_REQUIRED_PERMISSIONS) != null ) {
             String[] permissions = getIntent().getStringArrayExtra(EXTRA_REQUIRED_PERMISSIONS);
