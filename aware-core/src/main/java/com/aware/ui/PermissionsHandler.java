920b8b4fe39e73ac629caf6f188a716697318821
Denzil Ferreira <ferreiradenzil@gmail.com>
2015-12-03 09:58:29 +0200

    - Start AWARE with plugins - Fixed crash when stopping plugin - Apply new settings immediately - WIP: Fix multiple requests for the same permissions

--- aware-core/src/main/java/com/aware/ui/PermissionsHandler.java
+++ aware-core/src/main/java/com/aware/ui/PermissionsHandler.java
@@ -1,6 +1,7 @@
 package com.aware.ui;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
@@ -17,29 +18,63 @@ public class PermissionsHandler extends Activity {
     public static String EXTRA_REQUIRED_PERMISSIONS = "required_permissions";
     private final int CODE_PERMISSION_REQUEST = 999;
 
+    private static ArrayList<String> missing = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if( getIntent() != null && getIntent().getExtras() != null && getIntent().getStringArrayExtra(EXTRA_REQUIRED_PERMISSIONS) != null ) {
-
             String[] permissions = getIntent().getStringArrayExtra(EXTRA_REQUIRED_PERMISSIONS);
-
             //Check if we have requested this permission already
-            ArrayList<String> missing = new ArrayList<>();
             for( String p : permissions ) {
                 int ok = ContextCompat.checkSelfPermission(this, p);
-                if( ok != PackageManager.PERMISSION_GRANTED ) missing.add(p);
-            }
-            if( missing.size() > 0 ) {
-                ActivityCompat.requestPermissions(PermissionsHandler.this, missing.toArray(new String[missing.size()]), CODE_PERMISSION_REQUEST);
+                if( ok != PackageManager.PERMISSION_GRANTED && ! is_missing(p) ) missing.add(p);
             }
         }
-        finish();
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        if( intent != null && intent.getExtras() != null && intent.getStringArrayExtra(EXTRA_REQUIRED_PERMISSIONS) != null ) {
+            String[] permissions = intent.getStringArrayExtra(EXTRA_REQUIRED_PERMISSIONS);
+            //Check if we have requested this permission already
+            for( String p : permissions ) {
+                int ok = ContextCompat.checkSelfPermission(getApplicationContext(), p);
+                if( ok != PackageManager.PERMISSION_GRANTED && ! is_missing(p) ) missing.add(p);
+            }
+        }
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        for(String pp : missing ) {
+            if( ContextCompat.checkSelfPermission(getApplicationContext(), pp) == PackageManager.PERMISSION_GRANTED ) missing.remove(pp);
+        }
+        if( missing.size() == 0 ) {
+            finish();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if( missing.size() > 0 ) {
+            ActivityCompat.requestPermissions(PermissionsHandler.this, missing.toArray(new String[missing.size()]), CODE_PERMISSION_REQUEST);
+        }
         finish();
+    }
+
+    /**
+     * Check if we are already asking this permission
+     * @param p
+     * @return
+     */
+    private boolean is_missing( String p ) {
+        for( String pp : missing ) {
+            if( pp.equals(p) ) return true;
+        }
+        return false;
     }
 }
