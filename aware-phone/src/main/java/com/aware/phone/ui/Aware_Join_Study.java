05c33a366a32c651ea8e5fac5e451a91eee06de4
Juan Camilo García <juancamilo87@outlook.com>
2016-10-06 16:40:27 +0300

    UI creation

---
+++ aware-phone/src/main/java/com/aware/phone/ui/Aware_Join_Study.java
@@ -0,0 +1,41 @@
+package com.aware.phone.ui;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.TextView;
+
+import com.aware.phone.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Aware_Join_Study extends Aware_Activity {
+
+    private String study_url;
+    private JSONObject study_json;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.aware_join_study);
+
+        TextView txtStudyTitle = (TextView) findViewById(R.id.txt_title);
+        TextView txtStudyDescription = (TextView) findViewById(R.id.txt_description);
+        TextView txtStudyResearcher = (TextView) findViewById(R.id.txt_researcher);
+
+        study_url = getIntent().getStringExtra("study_url");
+        try {
+            study_json = new JSONObject(getIntent().getStringExtra("study_json"));
+            txtStudyTitle.setText((study_json.getString("study_name").length() > 0 ? study_json.getString("study_name") : "Not available"));
+            txtStudyDescription.setText((study_json.getString("study_description").length() > 0 ? study_json.getString("study_description") : "Not available."));
+            txtStudyResearcher.setText("PI: " + study_json.getString("researcher_first") + " " + study_json.getString("researcher_last") + "\nContact: " + study_json.getString("researcher_contact"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+
+    }
+}
